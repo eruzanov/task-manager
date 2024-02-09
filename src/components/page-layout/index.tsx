71c38c1fa1c412ae8c0943d462e379c171b57671
eruzanov <eugene.ruzanov@yandex.ru>
2024-02-09 22:39:39 +0300

    adds gihub link into Footer

--- src/components/page-layout/index.tsx
+++ src/components/page-layout/index.tsx
@@ -1,30 +1,41 @@
-import { Layout, ConfigProvider } from "antd";
+import { Layout, ConfigProvider, Button, Flex } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
 import { NavMenu } from "components/nav-menu";
+
+const GITHUB_LINK = "https://github.com/eruzanov/task-manager";
 
 interface PageLayoutProps {
     children: React.ReactNode;
 }
 
-export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
-    return (
-        <ConfigProvider
-            theme={{ components: { Layout: { headerPadding: "0 24px" } } }}
-        >
-            <Layout>
-                <Layout.Header
-                    style={{
-                        position: "sticky",
-                        top: 0,
-                        zIndex: 1,
-                        width: "100%",
-                    }}
-                >
-                    <NavMenu />
-                </Layout.Header>
-                <Layout.Content>
-                    <div style={{ margin: 24 }}>{children}</div>
-                </Layout.Content>
-            </Layout>
-        </ConfigProvider>
-    );
-};
+export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => (
+    <ConfigProvider
+        theme={{ components: { Layout: { headerPadding: "0 24px" } } }}
+    >
+        <Layout style={{ minHeight: "100vh" }}>
+            <Layout.Header
+                style={{
+                    position: "sticky",
+                    top: 0,
+                    zIndex: 1,
+                    width: "100%",
+                }}
+            >
+                <NavMenu />
+            </Layout.Header>
+            <Layout.Content style={{ margin: 24 }}>{children}</Layout.Content>
+            <Layout.Footer>
+                <Flex align="center" justify="center">
+                    <Button
+                        type="link"
+                        href={GITHUB_LINK}
+                        icon={<GithubOutlined />}
+                        size="large"
+                    >
+                        Github
+                    </Button>
+                </Flex>
+            </Layout.Footer>
+        </Layout>
+    </ConfigProvider>
+);
