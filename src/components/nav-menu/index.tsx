a57214d3ae6a86d66fabe5a1b241324a4af92b7a
eruzanov <eugene.ruzanov@yandex.ru>
2024-02-11 01:25:14 +0300

    upd NavMenu

--- src/components/nav-menu/index.tsx
+++ src/components/nav-menu/index.tsx
@@ -1,23 +1,34 @@
-import type { MenuProps } from "antd";
+import { useMemo } from "react";
 import { Menu } from "antd";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import {
     UnorderedListOutlined,
     ProjectOutlined,
     PlusOutlined,
 } from "@ant-design/icons";
 
-const items: MenuProps["items"] = [
-    { key: "/", label: "List of tasks", icon: <UnorderedListOutlined /> },
+const items = [
+    { key: "/", label: "Backlog", icon: <UnorderedListOutlined /> },
     { key: "/board", label: "Board", icon: <ProjectOutlined /> },
     { key: "/task", label: "Create Task", icon: <PlusOutlined /> },
 ];
 
 export const NavMenu = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const onClick = ({ key }: { key: string }) => navigate(key);
+    const current = useMemo(
+        () => items.find(({ key }) => key === location.pathname) ?? { key: "" },
+        [location.pathname]
+    );
 
     return (
-        <Menu theme="dark" mode="horizontal" onClick={onClick} items={items} />
+        <Menu
+            theme="dark"
+            mode="horizontal"
+            onClick={onClick}
+            items={items}
+            selectedKeys={[current.key]}
+        />
     );
 };
