49da350a17afec1eddef871b179c2a099bbbad90
eruzanov <eugene.ruzanov@yandex.ru>
2024-02-11 05:03:33 +0300

    adds useDateTime

--- src/components/board-card/index.tsx
+++ src/components/board-card/index.tsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
 import { Card, Divider } from "antd";
 import { EditOutlined, EyeOutlined } from "@ant-design/icons";
-import dayjs from "dayjs";
 
 import type { Task } from "features/tasks/types";
+import { useDateTime } from "features/datetime/useDateTime";
+import { DEFAULT_DATE_FORMAT } from "features/datetime/constants";
 import "./board-card.css";
-
-const DATE_FORMAT = "DD MMM YYYY";
 
 type BoardCardProps = Task;
 
@@ -15,17 +14,24 @@ export const BoardCard: React.FC<BoardCardProps> = ({
     title,
     description,
     updatedAt,
-}) => (
-    <Card title={title}>
-        {description && (
-            <div className="board-card__description">{description}</div>
-        )}
-        {updatedAt && <span className="board-card__updated">
-            <EditOutlined /> {dayjs(updatedAt).format(DATE_FORMAT)}
-        </span>}
-        <Divider type="vertical" />
-        <Link to={`/task/${id}`}>
-            <EyeOutlined /> {id}
-        </Link>
-    </Card>
-);
+}) => {
+    const datetime = useDateTime();
+
+    return (
+        <Card title={title}>
+            {description && (
+                <div className="board-card__description">{description}</div>
+            )}
+            {updatedAt && (
+                <span className="board-card__updated">
+                    <EditOutlined />{" "}
+                    {datetime(updatedAt).format(DEFAULT_DATE_FORMAT)}
+                </span>
+            )}
+            <Divider type="vertical" />
+            <Link to={`/task/${id}`}>
+                <EyeOutlined /> {id}
+            </Link>
+        </Card>
+    );
+};
